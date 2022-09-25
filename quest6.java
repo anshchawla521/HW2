0ba5ed0302f7b118bbaefeef42be805bb2fec5b5
anshchawla521 <anshchawla521@gmail.com>
2022-09-25 19:47:02 +0530

    question 6 done

---
+++ quest6.java
@@ -0,0 +1,12 @@
+public class quest6 {
+    public static void main(String args[])
+    {
+        int x = 3;
+        do 
+        {
+            System.out.print(x+" ");
+            x = x+3;
+        }
+        while(x<=36);
+    }
+}
