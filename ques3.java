3f90211fbc5d55731f900c4e574cf38e62669bd9
anshchawla521 <anshchawla521@gmail.com>
2022-09-25 19:26:53 +0530

    question 3

---
+++ ques3.java
@@ -0,0 +1,105 @@
+import javax.management.monitor.MonitorSettingException;
+
+public class ques3 {
+    public static void main(String args[])
+    {
+        int month = 04;
+
+        switch(month)
+        {
+            case 1:
+            // january
+            System.out.print("Month Name ");
+            System.out.println("January");
+            System.out.print("Number of days ");
+            System.out.println("31");
+            break;
+
+            case 2:
+            System.out.print("Month Name ");
+            System.out.println("February");
+            System.out.print("Number of days ");
+            System.out.println("28");
+            break;
+
+
+            case 3:
+            System.out.print("Month Name ");
+            System.out.println("March");
+            System.out.print("Number of days ");
+            System.out.println("31");
+            break;
+
+            case 4:
+            System.out.print("Month Name ");
+            System.out.println("April");
+            System.out.print("Number of days ");
+            System.out.println("30");
+            break;
+
+            case 5:
+            System.out.print("Month Name ");
+            System.out.println("May");
+            System.out.print("Number of days ");
+            System.out.println("31");
+            break;
+
+            case 6:
+            System.out.print("Month Name ");
+            System.out.println("June");
+            System.out.print("Number of days ");
+            System.out.println("30");
+            break;
+
+            case 7:
+            System.out.print("Month Name ");
+            System.out.println("July");
+            System.out.print("Number of days ");
+            System.out.println("31");
+            break;
+
+            case 8:
+            System.out.print("Month Name ");
+            System.out.println("August");
+            System.out.print("Number of days ");
+            System.out.println("31");
+            break;
+
+            case 9:
+            System.out.print("Month Name ");
+            System.out.println("September");
+            System.out.print("Number of days ");
+            System.out.println("30");
+            break;
+
+            case 10:
+            System.out.print("Month Name ");
+            System.out.println("October");
+            System.out.print("Number of days ");
+            System.out.println("31");
+            break;
+
+            case 11:
+            System.out.print("Month Name ");
+            System.out.println("November");
+            System.out.print("Number of days ");
+            System.out.println("30");
+            break;
+
+            case 12:
+            System.out.print("Month Name ");
+            System.out.println("December");
+            System.out.print("Number of days ");
+            System.out.println("31");
+            break;
+
+            default:
+            System.out.print("Month Name ");
+            System.out.println("NOT valid");
+            System.out.print("Number of days ");
+            System.out.println("Not valid");
+            break;
+        }
+    }
+    
+}
